20b810558e10d30cfcbf24abb86cb0e90e984aab
ronnaksaxena <rsaxena@hamilton.edu>
2021-01-18 18:50:19 -0500

    started working on double bottom

--- chartingTool.py
+++ chartingTool.py
@@ -16,11 +16,15 @@ SPX = SPX.tail(365)
 SPX.iloc[:,0] = pd.to_datetime(SPX.iloc[:,0], format = '%Y-%m-%d')
 SPX = SPX.set_index(pd.DatetimeIndex(SPX['Date']))
 '''
-
+'''
 ticker = input('Enter Ticker Symbol:')
 timeFrame = int(input('Enter Span of Chart in Days:'))
 start = dt.datetime.now() - dt.timedelta(days=timeFrame)
 end = dt.datetime.now()
+'''
+ticker = 'PFE'
+start = dt.datetime(1999,11,1)
+end = dt.datetime(2000,5,1)
 
 
 #gets data and restructures it
@@ -38,15 +42,16 @@ s  = mpf.make_mpf_style(marketcolors=mc)
 #get list of moving average values
 sma10 = list(data['Close'].rolling(10).mean())
 
+#test with F chart from 12/1/1998-12/1/1999
 #checks if trend is double top
 def isDoubleTop(sma10):
     trend = 'up'
     patternStarted = False
-    lastPrice = sma10[4]
+    lastPrice = sma10[9]
     #local extrema tuples are (Min/Max, index)
     firstLMax = secondLMax = (-1, -1)
     firstLMin = (float('inf'), -1)
-    for i in range(5,len(sma10),7):
+    for i in range(10,len(sma10),5):
         trend = 'up' if sma10[i] > lastPrice else 'down'
         lastPrice = sma10[i]
         if not patternStarted:
@@ -54,7 +59,7 @@ def isDoubleTop(sma10):
             if trend=='up':
                 firstLMax = (sma10[i], i)
             #check for signs of pattern starting
-            if trend=='down' and -1 < firstLMax[1]:
+            elif -1 < firstLMax[1]:
                 patternStarted = True
                 firstLMin = (sma10[i], i)
         #check if pattern meets requirements
@@ -70,12 +75,70 @@ def isDoubleTop(sma10):
                     patternStarted = False
                 else:
                     secondLMax = (sma10[i], i)
-            elif trend=='down' and secondLMax[1]>0:
+            elif trend=='down' and sma10[i] < firstLMin[0]:
                 #meets pattern if breaks support
-                if sma10[i] < firstLMin[0]:
+                return True
+    return False
+
+#test with PFE chart from 11/1/1999-05/1/200
+#checks if trend is double bottom
+def isDoubleBottom(sma10):
+    trend = 'down'
+    patternStarted = False
+    lastPrice = sma10[9]
+    #local extrema tuples are (Min/Max, index)
+    firstLMin = secondLMin = (float('inf'), -1)
+    firstLMax = (float('-inf'), -1)
+    for i in range(14,len(sma10),5):
+        trend = 'up' if sma10[i] > lastPrice else 'down'
+        lastPrice = sma10[i]
+        #trying to find firstLMin/support
+        if not patternStarted:
+            if trend=='down':
+                firstLMin = (sma10[i], i)
+            #check for signs of pattern starting
+            elif firstLMin[1] > 0:
+                patternStarted = True
+                firstLMax = (sma10[i], i)
+        #check if pattern meets requirements
+        else:
+            #try to find localMax/Resistance
+            if trend=='up' and secondLMin[1]<0:
+                firstLMax = (sma10[i], i)
+            elif trend=='down':
+                #checks if broke support too early then break pattern
+                if sma10[i] < (firstLMin[0] * 0.9):
+                    firstLMin = (float('inf'), -1)
+                    secondLMin = (float('inf'), -1)
+                    firstLMax = (float('-inf'), -1)
+                    patternStarted = False
+                else:
+                    secondLMin = (sma10[i],i)
+
+            #checks if second uptrend breaks resistance
+            elif trend=='up' and sma10[i]>firstLMax[0]:
+                #check if local mins are too far away
+                print(firstLMin)
+                print(firstLMax)
+                print(secondLMin)
+                if not (firstLMax[1] - firstLMin[1])*0.5 <= (secondLMin[1] - firstLMax[1]) <= (firstLMax[1] - firstLMin[1])*1.5:
+                    firstLMin = (float('inf'), -1)
+                    secondLMin = (float('inf'), -1)
+                    firstLMax = (float('-inf'), -1)
+                    patternStarted = False
+                #found pattern
+                else:
                     return True
     return False
 
+                
+
+
+
+            
+
+
+print(isDoubleBottom(sma10))
 
 #plots candlestick chart
 mpf.plot(data, **kwargs,style=s)
